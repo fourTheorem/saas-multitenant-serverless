433fad9c77cf1bfae3b17486ca03412dea8a6624
Guilherme Dalla Rosa <gui_dr@msn.com>
2024-05-08 10:54:59 +0100

    initial commit

---
+++ src/api/index.mjs
@@ -0,0 +1,43 @@
+import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
+import { QueryCommand } from '@aws-sdk/lib-dynamodb';
+
+export async function handler(event, context) {
+  console.log('event: ', event);
+
+  const credentials = event.requestContext.authorizer.lambda.credentials;
+  const tenant = event.queryStringParameters.tenant ?? 'default';
+
+  const ddbClient = new DynamoDBClient({
+    credentials: {
+      accessKeyId: credentials.AccessKeyId,
+      secretAccessKey: credentials.SecretAccessKey,
+      sessionToken: credentials.SessionToken,
+    },
+  });
+
+  try {
+    const response = await ddbClient.send(new QueryCommand({
+      TableName: 'saas-products-dev',
+      KeyConditionExpression: 'tenant = :tenant',
+      ExpressionAttributeValues: {
+        ':tenant': tenant,
+      },
+    }));
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        message: 'Hello World',
+        products: response.Items,
+      }),
+    };
+  } catch (error) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({
+        message: 'Error',
+        error: error,
+      }),
+    };
+  }
+}
---
+++ src/authorizer.mjs
@@ -0,0 +1,64 @@
+import { STSClient, AssumeRoleCommand } from '@aws-sdk/client-sts';
+
+const stsClient = new STSClient({ region: 'eu-west-1' });
+
+export async function handler(event, context) {
+  console.log('event: ', event);
+
+  const response = await stsClient.send(new AssumeRoleCommand({
+    RoleArn: 'arn:aws:iam::629383401309:role/authorizer-access-role',
+    RoleSessionName: 'authorizer-session',
+    DurationSeconds: 900,
+    Policy: JSON.stringify({
+      Version: '2012-10-17',
+      Statement: [
+        {
+          Effect: 'Allow',
+          Action: 'dynamodb:Query',
+          Resource: 'arn:aws:dynamodb:eu-west-1:629383401309:table/saas-products-dev',
+          // Condition: {
+          //   StringEquals: {
+          //     'dynamodb:LeadingKeys': [
+          //       '${aws:PrincipalTag/TenantId}',
+          //     ],
+          //   },
+          // },
+        },
+      ],
+    }),
+    Tags: [
+      {
+        Key: 'TenantId',
+        Value: 'chicken',
+      },
+    ],
+  }));
+
+  console.log(response);
+
+  return {
+    ...generatePolicy('user', 'Allow', '*'),
+    context: {
+      credentials: response.Credentials,
+    },
+  };
+
+  // return generatePolicy('user', 'Allow', 'arn:aws:execute-api:eu-west-1:665863320777:*/*');
+}
+
+const generatePolicy = (principalId, effect, resource) => {
+  const authResponse = {};
+  authResponse.principalId = principalId;
+  if (effect && resource) {
+    const policyDocument = {};
+    policyDocument.Version = '2012-10-17';
+    policyDocument.Statement = [];
+    const statementOne = {};
+    statementOne.Action = 'execute-api:Invoke';
+    statementOne.Effect = effect;
+    statementOne.Resource = resource;
+    policyDocument.Statement[0] = statementOne;
+    authResponse.policyDocument = policyDocument;
+  }
+  return authResponse;
+};
